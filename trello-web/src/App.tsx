5586cdb9f049ced40483299df8cca68de088e700
vietanh <vietanhle2662001@gmail.com>
2024-05-25 16:27:53 +0700

    add_theme_mui

--- trello-web/src/App.tsx
+++ trello-web/src/App.tsx
@@ -1,5 +1,13 @@
+import Typography from "@mui/material/Typography";
+
 function App() {
-  return <>Hello</>;
+  return (
+    <>
+      <div>Hello world</div>
+
+      <Typography variant="body2" color="text.secondary">Test Typo</Typography>
+    </>
+  );
 }
 
 export default App;
