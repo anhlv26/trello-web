31f35ea4088407c63b7b618d7e8ab412c5bbd92d
vietanh <vietanhle2662001@gmail.com>
2024-05-26 10:36:16 +0700

    dark_light_ver2

--- trello-web/src/App.tsx
+++ trello-web/src/App.tsx
@@ -1,13 +1,66 @@
+import { useMediaQuery } from "@mui/material";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import {
-  Experimental_CssVarsProvider as CssVarsProvider,
   experimental_extendTheme as extendTheme,
   useColorScheme,
 } from "@mui/material/styles";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import SettingsBrightnessIcon from "@mui/icons-material/SettingsBrightness";
+import Box from "@mui/system/Box";
+
+type Mode = "light" | "dark" | "system";
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme();
+
+  const handleChange = (event: SelectChangeEvent) => {
+    const selectedMode = event.target.value as Mode;
+    console.log("selectedMode ", selectedMode);
+    setMode(selectedMode);
+  };
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+            <LightModeIcon fontSize="small" /> Light
+          </Box>
+        </MenuItem>
+        <MenuItem value="dark">
+          <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+            <DarkModeOutlinedIcon fontSize="small" /> Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value="system">
+          <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+            <SettingsBrightnessIcon fontSize="small" /> System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  );
+}
 
 function ModeToggle() {
   const { mode, setMode } = useColorScheme();
+
+  // get system theme value
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const prefersLightMode = useMediaQuery("(prefers-color-scheme: light)");
   return (
     <Button
       onClick={() => {
@@ -26,6 +79,8 @@ const theme = extendTheme({
 function App() {
   return (
     <>
+      <ModeSelect />
+      <hr />
       <ModeToggle />
       <div>Hello world</div>
 
